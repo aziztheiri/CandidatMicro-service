888c3ccdc314868f542641457c1386fa5c2d3be3
aziztheiri <alatheiri123@gmail.com>
2025-02-07 13:58:54 +0100

    xx

--- src/main/java/com/esprit/ms/job/JobApplication.java
+++ src/main/java/com/esprit/ms/job/JobApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class JobApplication {
 
     public static void main(String[] args) {
